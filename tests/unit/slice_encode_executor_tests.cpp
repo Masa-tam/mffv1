99ecc2f3a0ed0033f4024acce183f19b6a6652ec
Masa-tam <31396809+Masa-tam@users.noreply.github.com>
2026-07-08 14:26:54 +0900

    Cover GR YUV420 multi-slice inter round trip

--- tests/unit/slice_encode_executor_tests.cpp
+++ tests/unit/slice_encode_executor_tests.cpp
@@ -30,6 +30,19 @@ mffv1::syntax::StreamParameters make_stream(
     return stream;
 }
 
+mffv1::syntax::StreamParameters make_yuv420_stream()
+{
+    auto stream = make_stream();
+    stream.width = 8;
+    stream.height = 4;
+    stream.chroma_planes = true;
+    stream.log2_h_chroma_subsample = 1;
+    stream.log2_v_chroma_subsample = 1;
+    stream.entropy_mode = mffv1::EntropyMode::GolombRice;
+    stream.intra_only = false;
+    return stream;
+}
+
 mffv1::PlaneView make_input_plane(const std::array<std::uint8_t, 8>& storage)
 {
     return {
@@ -95,6 +108,88 @@ void expect_stateful_round_trip(mffv1::EntropyMode entropy_mode)
     ASSERT_TRUE(decoder.decode(
         output, parsed_second.slices, parsed_second.keyframe).ok());
     EXPECT_EQ(decoded, second);
+}
+
+TEST(SliceEncodeExecutorTest, StatefulGolombRiceYuv420MultiSliceRoundTripsNonKeyframe)
+{
+    const auto stream = make_yuv420_stream();
+    const std::array<std::uint8_t, 32> first_y{
+        0, 11, 22, 33, 44, 55, 66, 77,
+        8, 19, 30, 41, 52, 63, 74, 85,
+        16, 27, 38, 49, 60, 71, 82, 93,
+        24, 35, 46, 57, 68, 79, 90, 101,
+    };
+    const std::array<std::uint8_t, 8> first_cb{
+        128, 132, 136, 140,
+        144, 148, 152, 156,
+    };
+    const std::array<std::uint8_t, 8> first_cr{
+        64, 68, 72, 76,
+        80, 84, 88, 92,
+    };
+    const std::array<std::uint8_t, 32> second_y{
+        101, 90, 79, 68, 57, 46, 35, 24,
+        93, 82, 71, 60, 49, 38, 27, 16,
+        85, 74, 63, 52, 41, 30, 19, 8,
+        77, 66, 55, 44, 33, 22, 11, 0,
+    };
+    const std::array<std::uint8_t, 8> second_cb{
+        156, 152, 148, 144,
+        140, 136, 132, 128,
+    };
+    const std::array<std::uint8_t, 8> second_cr{
+        92, 88, 84, 80,
+        76, 72, 68, 64,
+    };
+    const std::array<mffv1::PlaneView, 3> first_planes{{
+        {first_y.data(), {mffv1::PlaneRole::Y, mffv1::SampleFormat::UInt8, 8, 4, 8}},
+        {first_cb.data(), {mffv1::PlaneRole::Cb, mffv1::SampleFormat::UInt8, 4, 2, 4}},
+        {first_cr.data(), {mffv1::PlaneRole::Cr, mffv1::SampleFormat::UInt8, 4, 2, 4}},
+    }};
+    const std::array<mffv1::PlaneView, 3> second_planes{{
+        {second_y.data(), {mffv1::PlaneRole::Y, mffv1::SampleFormat::UInt8, 8, 4, 8}},
+        {second_cb.data(), {mffv1::PlaneRole::Cb, mffv1::SampleFormat::UInt8, 4, 2, 4}},
+        {second_cr.data(), {mffv1::PlaneRole::Cr, mffv1::SampleFormat::UInt8, 4, 2, 4}},
+    }};
+    const mffv1::FrameView first_input{first_planes.data(), first_planes.size()};
+    const mffv1::FrameView second_input{second_planes.data(), second_planes.size()};
+    mffv1::codec::SliceEncodeExecutor encoder(stream, 4);
+    std::vector<std::byte> first_frame;
+    std::vector<std::byte> second_frame;
+
+    ASSERT_TRUE(encoder.encode(first_input, true, first_frame).ok());
+    ASSERT_TRUE(encoder.encode(second_input, false, second_frame).ok());
+
+    const mffv1::codec::FrameParser parser(stream);
+    mffv1::codec::FrameDecodeContext parsed_first;
+    mffv1::codec::FrameDecodeContext parsed_second;
+    ASSERT_TRUE(parser.parse_with_range_header(first_frame, parsed_first).ok());
+    ASSERT_TRUE(parser.parse_with_range_header(second_frame, parsed_second).ok());
+    ASSERT_TRUE(parsed_first.keyframe);
+    ASSERT_FALSE(parsed_second.keyframe);
+
+    std::array<std::uint8_t, 32> decoded_y{};
+    std::array<std::uint8_t, 8> decoded_cb{};
+    std::array<std::uint8_t, 8> decoded_cr{};
+    std::array<mffv1::MutablePlaneView, 3> output_planes{{
+        {decoded_y.data(), {mffv1::PlaneRole::Y, mffv1::SampleFormat::UInt8, 8, 4, 8}},
+        {decoded_cb.data(), {mffv1::PlaneRole::Cb, mffv1::SampleFormat::UInt8, 4, 2, 4}},
+        {decoded_cr.data(), {mffv1::PlaneRole::Cr, mffv1::SampleFormat::UInt8, 4, 2, 4}},
+    }};
+    mffv1::MutableFrameView output{output_planes.data(), output_planes.size()};
+    mffv1::codec::SliceExecutor decoder(stream, 4);
+    ASSERT_TRUE(decoder.decode(output, parsed_first.slices, parsed_first.keyframe).ok());
+    EXPECT_EQ(decoded_y, first_y);
+    EXPECT_EQ(decoded_cb, first_cb);
+    EXPECT_EQ(decoded_cr, first_cr);
+
+    decoded_y.fill(0);
+    decoded_cb.fill(0);
+    decoded_cr.fill(0);
+    ASSERT_TRUE(decoder.decode(output, parsed_second.slices, parsed_second.keyframe).ok());
+    EXPECT_EQ(decoded_y, second_y);
+    EXPECT_EQ(decoded_cb, second_cb);
+    EXPECT_EQ(decoded_cr, second_cr);
 }
 
 TEST(SliceEncodeExecutorTest, ResolvesAndCapsWorkerCount)
